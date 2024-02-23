d4e8e12a56810b3ce191a5be3a1282e4b6553004
Nicholas Papierniak <nick.papierniak@gmail.com>
2024-02-23 09:38:30 -0600

    Added geocoder lookup

--- Map Hackwich/ContentView.swift
+++ Map Hackwich/ContentView.swift
@@ -20,19 +20,36 @@ struct ContentView: View {
     @StateObject var locationManager = LocationManager()
     @State private var userTrackingMode: MapUserTrackingMode = .follow
     @State private var places = [Place(name: "Barrington High School",
-                           coordinate: CLLocationCoordinate2D(
-                               latitude: 42.15704, longitude: -88.14812))]
+                                       coordinate: CLLocationCoordinate2D(
+                                        latitude: 42.15704, longitude: -88.14812))]
     var body: some View {
         Map(coordinateRegion: $region,
-                   interactionModes: .all,
-                   showsUserLocation: true,
-                   userTrackingMode: $userTrackingMode,
-                   annotationItems: places) { place in
-                   MapAnnotation(coordinate: place.coordinate,
-                                 anchorPoint: CGPoint(x: 0.5, y: 1.2)) {
-                       Marker(name: place.name)
-                   }
-               }
+            interactionModes: .all,
+            showsUserLocation: true,
+            userTrackingMode: $userTrackingMode,
+            annotationItems: places) { place in
+            MapAnnotation(coordinate: place.coordinate,
+                          anchorPoint: CGPoint(x: 0.5, y: 1.2)) {
+                Marker(name: place.name)
+            }
+        }
+            .onAppear {
+                findLocation(name: "Springfield")
+            }
+    }
+    
+    func findLocation(name: String) {
+        locationManager.geocoder.geocodeAddressString(name) { (placemarks, error) in
+            guard placemarks != nil else {
+                print("Could not locate \(name)")
+                return
+            }
+            for placemark in placemarks! {
+                let place = Place(name: "\(placemark.name!), \(placemark.administrativeArea!)",
+                                  coordinate: placemark.location!.coordinate)
+                places.append(place)
+            }
+        }
     }
 }
 
